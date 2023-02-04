fd43e9b995c5187f611645b21d7eaee124eb7fac
Aryan Sharma <panditaryan.as@gmail.com>
2023-02-04 07:10:19 +0530

    intro

---
+++ 1.Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: 'aryan', age: 24 };
+console.log("hello world");
+console.log(user.name);
---
+++ 1.Intro/intro.ts
@@ -0,0 +1,4 @@
+let user = { name: 'aryan', age: 24 };
+
+console.log("hello world");
+console.log(user.name);
